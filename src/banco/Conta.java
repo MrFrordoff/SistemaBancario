04616e5762e27dab18d3656bbe10f6f36c74e042
MrFrordoff <82807976+MrFrordoff@users.noreply.github.com>
2024-07-04 16:06:24 -0300

    Conta.java

--- src/banco/Conta.java
+++ src/banco/Conta.java
@@ -1,2 +1,34 @@
-package banco;public class Conta {
+package banco;
+
+public class Conta {
+    private String titular;
+    private double saldo;
+
+    public Conta(String titular, double saldoInicial) {
+        this.titular = titular;
+        this.saldo = saldoInicial;
+    }
+
+    public synchronized void depositar(double valor) {
+        saldo += valor;
+        System.out.println(titular + " depositou: R$ " + valor);
+    }
+
+    public synchronized boolean sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+            System.out.println(titular + " sacou: R$ " + valor);
+            return true;
+        }
+        System.out.println(titular + " tentou sacar R$ " + valor + " mas saldo insuficiente.");
+        return false;
+    }
+
+    public synchronized double getSaldo() {
+        return saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
 }
